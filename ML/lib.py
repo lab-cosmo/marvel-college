8dbb9bf50f198c6c5cd55fbdf7e1f9d2dd88ac79
THIBAULT SOHIER <thibaultsohier@tsf-476-wpa-6-183.epfl.ch>
2018-05-02 13:42:57 +0200

    cleaned up bulk modulus exercise

--- ML/lib.py
+++ ML/lib.py
@@ -2,16 +2,16 @@ import ipywidgets
 import nglview as nv
 import ase
 
-def view(sdict, tu):
+def view(sdict, rep=(1,1,1)):
     ase_str=ase.Atoms(symbols=sdict['species'], positions=sdict['positions'], cell=sdict['cell'])
-    return ase_view(ase_str*tu)
-def ase_view(s):
-    view=nv.show_ase(s, gui=True)
+    return ase_view(ase_str*rep)
+def ase_view(s, gui=False):
+    view=nv.show_ase(s, gui=gui)
     view.clear_representations()
     view.add_ball_and_stick(aspectRatio=4)
     return view
 def traj_view(t):
-    view=nv.show_asetraj(t, gui=True)
+    view=nv.show_asetraj(t)
     view.clear_representations()
     view.add_ball_and_stick(aspectRatio=4)
     return view
